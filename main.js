8a1683c6f29a36c59f050d283a7805deafa21932
Paula Robles <paulag.robles.sosa@gmail.com>
2020-11-25 21:53:07 -0600

    Maquetacion final

---
+++ main.js
@@ -0,0 +1,35 @@
+function fnc1() {
+    document.getElementById("diss").style.display="flex";
+    document.getElementById("diss2").style.display="none";
+    document.getElementById("diss3").style.display="none";
+    document.getElementById("diss4").style.display="none";
+    document.getElementById("diss5").style.display="none";
+}
+function fnc2() {
+    document.getElementById("diss").style.display="none";
+    document.getElementById("diss2").style.display="flex";
+    document.getElementById("diss3").style.display="none";
+    document.getElementById("diss4").style.display="none";
+    document.getElementById("diss5").style.display="none";
+}
+function fnc3() {
+    document.getElementById("diss").style.display="none";
+    document.getElementById("diss2").style.display="none";
+    document.getElementById("diss3").style.display="flex";
+    document.getElementById("diss4").style.display="none";
+    document.getElementById("diss5").style.display="none";
+}
+function fnc4() {
+    document.getElementById("diss").style.display="none";
+    document.getElementById("diss2").style.display="none";
+    document.getElementById("diss3").style.display="none";
+    document.getElementById("diss4").style.display="flex";
+    document.getElementById("diss5").style.display="none";
+}
+function fnc5() {
+    document.getElementById("diss").style.display="none";
+    document.getElementById("diss2").style.display="none";
+    document.getElementById("diss3").style.display="none";
+    document.getElementById("diss4").style.display="none";
+    document.getElementById("diss5").style.display="flex";
+}
